f99127a345991ab3c7959358f641f85f6f12e357
Matheus Fernando Vieira Pinto <vieira.matheusf13@gmail.com>
2023-08-21 17:53:53 -0300

    funções sacar, depoistar e verificarExtrato criadas

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -8,46 +8,66 @@ display = """
 ====================================================================
 """
 
-QUANTIDADE_DE_SAQUES_DIARIO = 3
-LIMITE_DE_SAQUE = 500.00
-saldo = 0.00
-quantidade_de_saques_realizados = 0 
-extrato = ""
+def depositar(saldo, valor, extrato, /):
+    if valor <= 0:
+        print("Erro: O valor a ser depositado deve ser maior que 0.")
+    else:
+        saldo += valor
+        extrato += f"Deposito - R${valor:.2f}\n"
+        print("Deposito realizado com sucesso!")
+    return saldo, extrato 
 
-while True:
-    opcao = int(input(display))
-    os.system('clear') or None
-    if opcao == 1:
-        valor = float(input("Informe o valor a ser depositado: "))
-        if valor <= 0.00:
-            print("Erro - O valor a ser depositado não pode ser negativo!")
+def sacar(*, saldo, valor, extrato, limite_saque, quantidade_de_saques_realizados, limite_saques):
+    if quantidade_de_saques_realizados >= limite_saques:
+        print("Erro - Você atingiu a quantidade maxima de saques diários!") 
+    else:
+        valor = float(input("Informe o valor a ser sacado: ")) 
+        if(valor > saldo):
+            print("Erro - Você não possui saldo suficiente")
+        elif valor >= limite_saque:
+            print(f"Erro - O limite maximo por saque é de R${limite_saque:.2f}")
         else:
-            saldo += valor
-            extrato += f"Deposito - R${valor:.2f}\n"
-            print("Deposito realizado com sucesso!") 
-    elif opcao == 2:
-        if quantidade_de_saques_realizados >= QUANTIDADE_DE_SAQUES_DIARIO:
-            print("Erro - Você atingiu a quantidade maxima de saques diários! Tente") 
-        else:
-            valor = float(input("Informe o valor a ser sacado: ")) 
-            if(valor > saldo):
-                print("Erro - Você não possui saldo suficiente")
-            else:
-                saldo -= valor
-                quantidade_de_saques_realizados += 1
-                extrato += f"Saque - R${valor:.2f}\n"
-                print("Saque realizado com sucesso!")    
-    elif opcao == 3:
+            saldo -= valor
+            quantidade_de_saques_realizados += 1
+            extrato += f"Saque - R${valor:.2f}\n"
+            print("Saque realizado com sucesso!")
+    return saldo, extrato, quantidade_de_saques_realizados
+    
+def verificarExtrato(saldo, /, *, extrato):
+    if extrato == "":
+        print("Não houve movimentações na conta.")
+    else:
         print("EXTRATO".center(19, "#"))
         print(extrato)
         print(f"Saldo em conta - R${saldo:.2f}")
         print("#".center(19, "#"))
-    elif opcao == 0:
-        break
-    else:
-        print("Opção Inválida! Tente novamente.")
-    
-        
+
+def main():
+    QUANTIDADE_DE_SAQUES_DIARIO = 3
+    LIMITE_DE_SAQUE = 500.00
+    saldo = 0.00
+    quantidade_de_saques_realizados = 0 
+    extrato = ""
+    while True:
+        opcao = int(input(display))
+        os.system('clear') or None
+        if opcao == 1:
+            valor = float(input("Informe o valor a ser depositado: "))
+            saldo, extrato = depositar(saldo, valor, extrato)
+                
+        elif opcao == 2:
+            saldo, extrato, quantidade_de_saques_realizados = sacar(saldo=saldo, valor=valor, extrato=extrato, limite_saque=LIMITE_DE_SAQUE, quantidade_de_saques_realizados=quantidade_de_saques_realizados, limite_saques=QUANTIDADE_DE_SAQUES_DIARIO)
+                    
+        elif opcao == 3:
+            verificarExtrato(saldo, extrato=extrato)
+        elif opcao == 0:
+            break
+        else:
+            print("Opção Inválida! Tente novamente.")
+            
+main()
+
+
     
 
 
